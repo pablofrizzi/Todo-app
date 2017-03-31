92a234222ffd0c35bbcf6c519e6f960930487845
pablofrizzi <pablofrizzi@gmail.com>
2017-03-31 15:11:17 +0200

    v1.0

---
+++ js/script.js
@@ -0,0 +1,177 @@
+$(document).ready(main);
+
+function main() {
+    var todos = [
+        {
+            task: 'Dodawanie TODO',
+            isCompleted: false
+        },
+        {
+            task: 'Dodany TODOS nie pojawia się po prostu a wyjeżdża w dowolnej animacji',
+            isCompleted: true
+        },
+        {
+            task: 'Przycisk edycji TODOSA (text zmienia się w input, przycisk edycji w zatwierdzenie), zmieniamy i zapisujemy konkretnego todosa ',
+            isCompleted: true
+        },
+        {
+            task: 'Usuwanie todosa, również z animacją',
+            isCompleted: true
+        },
+        {
+            task: 'Pusta lista wyświetla informacje "Brak zadań"',
+            isCompleted: true
+        }
+        
+    ];
+    
+    var app = {
+        
+        currentTask: null,
+        
+        showTodos: function() {
+            if(todos.length === 0) {
+                $('#no-todo').show();
+                $('.table').hide();
+            }  else {
+                $('#no-todo').hide();
+                $('.table').show();
+            }
+            var todosListEl = $('#todos-list');
+
+            todosListEl.html('');
+
+            todos.forEach(function(todo){
+                var taskClasses = 'todo-task';
+                if(todo.isCompleted) {
+                    taskClasses += ' is-completed';
+                }
+
+                todosListEl.append('\
+                    <tr>\
+                        <td class="' + taskClasses + '">' + todo.task + '</td>\
+                        <td>\
+                            <button class="btn btn-info edit-button">Edit</button>\
+                            <button class="btn btn-danger delete-button">Delete</button>\
+                            <button class="btn btn-success save-button">Save</button>\
+                            <button class="btn btn-danger cancel-button">Cancel</button>\
+                        </td>\
+                    </tr>\
+                ');              
+            });
+                  
+        },
+        
+        addTodo: function(event) {
+            event.preventDefault();
+            
+            var createInput = $('#create-input');
+            var createInputValue = createInput.val();
+            
+            var errorMessage = null;
+            
+            if(!createInputValue) {
+                errorMessage = 'Task cannot be empty.';
+            } else {
+                todos.forEach(function(todo) {
+                    if (todo.task === createInputValue) {
+                        errorMessage = 'Task already exists.'
+                    }
+                });
+            }
+            
+            if(errorMessage) {
+                app.showError(errorMessage);
+                return;
+            }
+            
+            todos.push({
+                task: createInputValue,
+                isCompleted: false
+            });
+            
+            createInput.val('');
+            app.showTodos();
+        },
+        
+        toggleTodo: function() {
+            todos.forEach(function(todo) {
+                if(todo.task === $(this).text()) {
+                    todo.isCompleted = !todo.isCompleted;
+                }
+            }.bind(this));
+            app.showTodos(); 
+        },
+        
+        enterEditMode: function() {
+            var actionsCell = $(this).closest('td');
+            var taskCell = actionsCell.prev();
+            
+            actionsCell.find('.save-button').show();
+            actionsCell.find('.cancel-button').show();
+            actionsCell.find('.edit-button').hide();
+            actionsCell.find('.delete-button').hide();
+            
+            taskCell.removeClass('todo-task');
+            app.currentTask = taskCell.text();
+            taskCell.html('<input type="text" class="edit-input" value"' + app.currentTask + '" />');
+        },
+        
+        exitEditMode: function() {
+            var actionsCell = $(this).closest('td');
+            var taskCell = actionsCell.prev();
+            
+            actionsCell.find('.save-button').hide();
+            actionsCell.find('.cancel-button').hide();
+            actionsCell.find('.edit-button').show();
+            actionsCell.find('.delete-button').show();
+            
+            taskCell.addClass('todo-task');
+            taskCell.html(app.currentTask);
+        },
+        
+        saveTask: function() {
+            var newTask = $('.edit-input').val();
+            
+            todos.forEach(function(todo) {
+                if(app.currentTask === todo.task) {
+                    todo.task = newTask;
+                }
+            });
+            app.currentTask = newTask;
+            app.exitEditMode.call(this);  
+        },
+        
+        deleteTask: function() {
+            var taskToDelete = $(this).parent('td').prev().text();
+            var found = false;
+            todos.forEach(function(todo, index) {
+                if(!found && taskToDelete === todo.task) {
+                    todos.splice(index, 1);
+                    found = true;
+                }
+            });
+            app.showTodos();
+        },
+        
+        showError: function(errorMessage) {
+            $('.error-message').html(errorMessage).slideDown();
+        },
+        
+        clearError: function() {
+            $('.error-message').fadeOut();
+        }
+    };
+        
+    app.showTodos();
+    
+    // $('.todo-task').on('click', app.toggleTodo);
+    $('#create-form').on('submit', app.addTodo);
+    $('#create-input').on('keyup', app.clearError);
+    $('table').on('click', '.todo-task', app.toggleTodo);
+    $('table').on('click', '.edit-button', app.enterEditMode);
+    $('table').on('click', '.cancel-button', app.exitEditMode);
+    $('table').on('click', '.save-button', app.saveTask);
+    $('table').on('click', '.delete-button', app.deleteTask);
+    
+};
